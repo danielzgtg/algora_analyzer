27eadb76275995053cd04916eeb4d452d0566a3b
Daniel Tang <danielzgtg.opensource@gmail.com>
2026-02-17 06:23:37 -0500

    Implement analysis and output

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { promises as fs } from "fs";
+import assert from "assert";
 
 await fs.mkdir("cache", { recursive: true });
 const bountiesJSON = await (async () => {
@@ -53,7 +54,12 @@ async function cacheFetch(path, url) {
 
 const urlRegex = /^https:\/\/github.com\/([a-zA-Z0-9-]{3,39})\/([A-Za-z0-9_.-]{2,100})\/issues\/(\d+)$/;
 const headlineRegex = /[A-Z]{2}\n([a-zA-Z0-9 ().,-]{3,39})\n#(\d+)\n\$([0-9,]+)\n([\][a-zA-Z0-9 ().,_:\/+<>`"=?\p{Emoji_Presentation}#'&-]+)$/u;
+const redundantRegex = /submitted a.+pull request.+that claims the bounty. You can.+visit your .+board.+to reward\.|The user @[a-zA-Z0-9-]{3,39} is already attempting to complete issue #\d+ and claim the bounty\. We recommend checking in on @[a-zA-Z0-9-]{3,39}'s progress, and potentially collaborating, before starting a new solution\.|@[a-zA-Z0-9-]{3,39}: Reminder that in 7 days the bounty will become up for grabs, so please submit a pull request before then|The bounty is up for grabs! Everyone is welcome to `\/attempt #\d+`|team prefers to assign a single contributor to the issue rather than let anyone attempt it right away. We recommend waiting for a confirmation from a member before getting started\.|@[a-zA-Z0-9-]{3,39}: Another person is already attempting this issue\. Please don't start working on this issue unless you were explicitly asked to do so\.|Note: The user @[a-zA-Z0-9-]{3,39} is already attempting to complete issue #\d+ and claim the bounty\. If you attempt to complete the same issue, there is a chance that @[a-zA-Z0-9-]{3,39} will complete the issue first, and be awarded the bounty\. We recommend discussing with @[a-zA-Z0-9-]{3,39} and potentially collaborating on the same solution versus creating an alternate solution\./;
+const attemptRegex = / @([a-zA-Z0-9-]{3,39}) \|| @([a-zA-Z0-9-]{3,39})<\/td>\n/g;
+const pullRequestRegex = /\| #(\d+) \| \[Reward\]\(|pull\/\d+">#(\d+)<\/a><\/td>\n/g;
+const allIssueNumberLikeRegex = /#(\d+)/g;
 
+const results = [];
 if (!Array.isArray(bountiesJSON)) throw Error;
 for (const bountyJSON of bountiesJSON) {
     const [url, headline] = bountyJSON;
@@ -91,5 +97,47 @@ for (const bountyJSON of bountiesJSON) {
         commentsJSON.push(...commentsPage);
         if (commentsPage.length !== 100) break;
     }
+
+    let createdAt;
+    const pullRequests = [];
+    const attempts = [];
+    for (const comment of commentsJSON) {
+        const { body, user } = comment;
+        if (!user.login.toLowerCase().includes("algora")) continue;
+        if ((user.id !== 121443259 || user.type !== "Bot") && (user.id !== 136125894 || user.type !== "User")) throw Error;
+        if (redundantRegex.test(body)) continue; // Redundant
+        if (createdAt) {
+            // Redirects inside transferred issues like https://github.com/tscircuit/3d-viewer/issues/534
+            if (pullRequests.length || attempts.length) throw new Error;
+        }
+        createdAt = new Date(comment.created_at).getTime();
+        for (const [attemptRaw, attemptUsername, attemptUsernameAlt] of body.matchAll(attemptRegex)) {
+            attempts.push(attemptUsername ?? attemptUsernameAlt);
+        }
+        attempts.sort();
+        for (const [pullRequestRaw, pullRequestId, pullRequestIdAlt] of body.matchAll(pullRequestRegex)) {
+            pullRequests.push(+(pullRequestId ?? pullRequestIdAlt));
+        }
+        pullRequests.sort();
+        const checkNumbers = [];
+        for (const [checkRaw, checkNumStr] of body.matchAll(allIssueNumberLikeRegex)) {
+            const checkNum = +checkNumStr;
+            if (checkNum === issueNum) continue;
+            checkNumbers.push(checkNum);
+        }
+        checkNumbers.sort();
+        assert.deepStrictEqual(pullRequests, checkNumbers);
+        if (pullRequests.length > attempts.length) throw new Error;
+    }
+    if (!createdAt) {
+        // Bot prevented from comments at https://github.com/rafael-fuente/diffractsim/issues/69
+        continue;
+    }
+
+    results.push([price, issueNum, ownerUsername, repo, attempts.length, pullRequests.length, description]);
 }
 
+console.log("price, issueNum, ownerUsername, repo, attempts.length, pullRequests.length, description")
+for (const result of results) {
+    console.log(`${result[0]},${result[1]},${result[2]},${result[3]},${result[4]},${result[5]},"${result[6].replaceAll('"', '""')}"`)
+}
